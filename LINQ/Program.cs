3bdb1bf84ced141e365f9f63f27fb41f856010f1
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-20 12:31:34 -0400

    copmpleted linq exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGamesNames = new List<string>() { "Call Of Duty: Black Ops 1", "Call Of Duty: Black Ops 2", "Call Of Duty: Black Ops 3" };
+
+            videoGamesNames.Add("Call Of Duty: Modern Warfare 2");
+            videoGamesNames.Add("Call Of Duty: Modern Warfare 3");
+
+            var orderedVideoGames = videoGamesNames.OrderBy(name => name.Length);
+            foreach (var videoGame in orderedVideoGames)
+            {
+                Console.WriteLine(videoGame);
+            }
+
         }
+
+
     }
 }
